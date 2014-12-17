fa2aeed440eaafcbd620e7a2bcfd736afc145ae9
Alexander <a.mazyrin@axitech.ru>
2014-12-17 16:52:35 +0300

    [*] Added some features in script

--- bash_scripts/create_proj.sh
+++ bash_scripts/create_proj.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# TODO: Check count and correctness of args.
+# This script creates simple c or cpp makefile project.
+# Author: Mazyrin Alexander.
+
+if [ $# -ne 2 ]
+then
+    echo "Bad args. Usage $0 <PROJ_NAME> <PROJ_TYPE(c or cpp)>."
+    exit 1
+fi
 
 WORK_DIR=`pwd`/$1
 PATTERNS_DIR="/home/mazyrin/testing_stuff/patterns"
@@ -9,23 +16,24 @@ CPP_SOURCE=$PATTERNS_DIR/main_pattern.cpp
 C_MAKE=$PATTERNS_DIR/c_makefile/Makefile
 CPP_MAKE=$PATTERNS_DIR/cpp_makefile/Makefile
 
-
 mkdir $WORK_DIR
 
-#TODO: Replace if on switch
-if [ $2 = "c" ]
-then 
-    SOURCE_FILE=$C_SOURCE
-    MAKE_FILE=$C_MAKE
-else
-    SOURCE_FILE=$CPP_SOURCE
-    MAKE_FILE=$CPP_MAKE
-fi
+case $2 in
+    c)
+        MAKE_FILE=$C_MAKE
+        cp $C_SOURCE $WORK_DIR/$1.c
+        ;;
+    cpp)
+        MAKE_FILE=$CPP_MAKE
+        cp $CPP_SOURCE $WORK_DIR/$1.cpp
+        ;;
+    *)
+        echo "Bad project type. Use \"c\" or \"cpp\"."
+        exit 1
+        ;;
+esac
 
-
-cp $SOURCE_FILE $WORK_DIR/$1.c
 cp $MAKE_FILE $WORK_DIR/
-
 
 exit 0
 
