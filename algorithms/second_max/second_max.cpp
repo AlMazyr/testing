e4cc4662948792f4d78251aa05c49e6d2d9527b1
Alex <mazyrinhome@mail.ru>
2015-02-07 16:22:42 +0300

    [*] Added count of maximums.

--- algorithms/second_max/second_max.cpp
+++ algorithms/second_max/second_max.cpp
@@ -3,24 +3,33 @@
 #include <utility>
 
 template<class T> 
-std::pair<T,T> find_two_max(const std::vector<T> &arr) {
-	std::pair<T, T> maxs(0, 0);
+std::pair<std::pair<T, int>, std::pair<T, int> > find_two_max(const std::vector<T> &arr) {
+	std::pair<std::pair<T, int>, std::pair<T, int> > maxs(std::make_pair(0, 0), std::make_pair(0, 0));
 	for(const auto i : arr) {
-		if(i > maxs.first){
-			maxs.second = maxs.first;
-		   	maxs.first = i;
+		if(i > maxs.first.first){
+			maxs.second.first = maxs.first.first;
+		   	maxs.first.first = i;
+			maxs.second.second = maxs.first.second;
+			maxs.first.second = 1;
+		} else if(i == maxs.first.first) {
+			maxs.first.second++;
+		} else if(i > maxs.second.first) { 
+			maxs.second.first = i;
+			maxs.second.second = 1;
+		} else if(i == maxs.second.first) {
+			maxs.second.second++;
 		}
-		else if(i > maxs.second) maxs.second = i;
 	}
 	return maxs;
 }
 
 
 int main(int argc, char *argv[]) {
-	std::vector<int> arr = {10, 11, 2, 4, 54, 2, 0};
-	std::pair<int, int> answ = find_two_max(arr);
+	std::vector<int> arr = {10, 11, 2, 11, 4, 54, 2, 54, 5, 7, 54, 54};
+	std::pair<std::pair<int, int>, std::pair<int, int> > answ = find_two_max(arr);
 	
-	std::cout << "The first max = " << answ.first << ", second max = " << answ.second << std::endl;	
+	std::cout << "First max = " << answ.first.first << ", count =  "<< answ.first.second << std::endl;
+	std::cout << "Second max = " << answ.second.first << ", count = "<< answ.second.second << std::endl;	
 
 	return 0;
 }
