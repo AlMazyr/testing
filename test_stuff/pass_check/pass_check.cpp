3965dc4eafd08f581faf699b93b2c9f963de5e7c
Alex <mazyrinhome@mail.ru>
2015-05-11 19:34:28 +0300

    [+] Added SHAD first programm.

---
+++ test_stuff/pass_check/pass_check.cpp
@@ -0,0 +1,16 @@
+// Programm reads string form  stdin.
+// If string contains uppercase and lowercase characters and digits,
+// then programm writes yes to stdout, else no.
+// String can contains only latin characters and digits.
+// Author: A. Mazyrin, algoritmist1618@gmail.com
+
+#include <iostream>
+
+
+int main(int argc, char *argv[]) {
+		
+	
+
+
+	return 0;
+}
