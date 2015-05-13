1c0d65582d59e61f87972b392aea0ef0a286ddf1
Alexander <a.mazyrin@axitech.ru>
2015-05-13 10:29:12 +0300

    [*] Double ridding of last char fixed.

--- test_stuff/pass_check/pass_check.cpp
+++ test_stuff/pass_check/pass_check.cpp
@@ -5,6 +5,7 @@
 // Author: A. Mazyrin, algoritmist1618@gmail.com
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -12,25 +13,27 @@ int main(int argc, char *argv[]) {
 
 	bool is_lower = false, is_upper = false, is_digit = false;
 
+	ifstream f_in("input.txt");
+	ofstream f_out("output.txt");
+
 	char x = 0;
 	int len = 0;
-	while((x = cin.get()) != '\n') {
+	while(f_in.get(x)) {
 		len++;
-		if ((x >= 48) && (x <= 57)) {
+		if ((x >= '0') && (x <= '9')) {
 			is_digit = true;
-		} else if((x >= 97) && (x <= 122)) {
+		} else if((x >= 'a') && (x <= 'z')) {
 			is_lower = true;
-		} else if((x >= 65) && (x <= 90)) {
+		} else if((x >= 'A') && (x <= 'Z')) {
 			is_upper = true;
 		}
 
 		if((is_digit == true) && (is_lower == true) && (is_upper == true) && (len >= 8)) {
-			cout << "yes";
+			f_out << "yes";
 			return 0;
 		}
 	}
 
-	cout << "no";
-
+	f_out << "no";
 	return 0;
 }
