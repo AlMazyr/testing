082739c37642051d828996a2cf11eb5668d2d118
Alex <mazyrinhome@mail.ru>
2016-12-18 11:23:28 +0300

    [*] Added stuff.

--- test_stuff/points/points.cpp
+++ test_stuff/points/points.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <cmath>
 
+#include "points.h"
+
+using namespace std;
+
+class Point {
+	int x;
+	int y;
+	int Balls() {
+		return 0;
+	}
+};
+
+int foo(Point p_current) {
+	return p_current.y * p_current.y;
+}
 
 int main(int argc, char *argv[]) {
+
+	int count_2 = 0;
+	Point p_current;
+	map<int, Point> points_arr;
+	int vector_len = 0;
+
+	cin >> count_2;
+
+	for(int i = 0; i < count_2; ++i) {
+		cin >> p_current.x;
+		cin >> p_current.y;
+		int sq = p_current.x * p_current.x;
+		vector_len = sqrt((sq) + (foo(p_current)));
+		points_arr[vector_len] = p_current;
+	}
+
+	for(map<int, Point>::iterator it = points_arr.begin(); it != points_arr.end(); ++it) {
+		cout << it->second.x << ' ' << it->second.y << '\n';
+	}
 
 	return 0;
 }
