bea6771996ecd32ff5c775a253c5a8a2e440ccf2
Alexander Mazyrin <algoritmist1618@gmail.com>
2017-08-06 15:21:20 +0300

    [+] Add 2 scripts for simple QEMU launch.

---
+++ bash_scripts/qemu_simple.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author: Mazyrin Alexander <algoritmist1618@gmail.com>
+
+if [ $# -ne 2 ]
+then
+	echo "Bad args. Usage $0 KERNEL INITRD"
+	exit 1
+fi
+
+qemu-system-x86_64 -enable-kvm -kernel $1 -initrd $2 -append 'console=ttyS0' -nographic
